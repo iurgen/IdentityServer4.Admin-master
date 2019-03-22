6afaad67aa71e000ebd49df6d6a7c7af039a6bdf
chenzhengyang <czy@DESKTOP-BLQR390>
2019-03-22 14:49:54 +0800

    添加项目文件。

---
+++ src/Skoruba.IdentityServer4.STS.Identity/Helpers/StartupHelpers.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Serilog;
+using Skoruba.IdentityServer4.STS.Identity.Configuration.Constants;
+using Skoruba.IdentityServer4.STS.Identity.Filters;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+namespace Skoruba.IdentityServer4.STS.Identity.Helpers
+{
+    public static class StartupHelpers
+    {
+        public static void AddMvcLocalization(this IServiceCollection services)
+        {
+            services.AddLocalization(opts => { opts.ResourcesPath = ConfigurationConsts.ResourcesPath; });
+
+            services.AddMvc(options => { options.Filters.Add(typeof(ApiResultAttribute)); })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddViewLocalization(
+                    LanguageViewLocationExpanderFormat.Suffix,
+                    opts => { opts.ResourcesPath = ConfigurationConsts.ResourcesPath; })
+                .AddDataAnnotationsLocalization();
+
+            services.Configure<RequestLocalizationOptions>(
+                opts =>
+                {
+                    var supportedCultures = new[]
+                    {
+                        new CultureInfo("ru"),
+                        new CultureInfo("en"),
+                        new CultureInfo("zh")
+                    };
+
+                    opts.DefaultRequestCulture = new RequestCulture("en");
+                    opts.SupportedCultures = supportedCultures;
+                    opts.SupportedUICultures = supportedCultures;
+                });
+        }
+
+        public static void UseSecurityHeaders(this IApplicationBuilder app)
+        {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions()
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseXfo(options => options.SameOrigin());
+            app.UseReferrerPolicy(options => options.NoReferrer());
+        }
+
+        public static void AddAuthenticationServices<TContext, TUserIdentity, TUserIdentityRole>(this IServiceCollection services, IHostingEnvironment hostingEnvironment, IConfiguration configuration, ILogger logger) where TContext : DbContext
+            where TUserIdentity : class where TUserIdentityRole : class
+        {
+            var connectionString = configuration.GetConnectionString(ConfigurationConsts.AdminConnectionStringKey);
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddIdentity<TUserIdentity, TUserIdentityRole>()
+                .AddEntityFrameworkStores<TContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IISOptions>(iis =>
+            {
+                iis.AuthenticationDisplayName = "Windows";
+                iis.AutomaticAuthentication = false;
+            });
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+            })
+                .AddAspNetIdentity<TUserIdentity>()
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => { sql.MigrationsAssembly(migrationsAssembly); sql.UseRowNumberForPaging(); });
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => { sql.MigrationsAssembly(migrationsAssembly); sql.UseRowNumberForPaging(); });
+                    options.EnableTokenCleanup = true;
+#if DEBUG
+                    options.TokenCleanupInterval = 15;
+#endif                
+                });
+
+
+            if (hostingEnvironment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                builder.AddSigningCredential(new X509Certificate2(Path.Combine(basePath, "certificate\\cas.service.pfx"), "123456"));
+                //throw new Exception("need to configure key material");
+            }
+            builder.AddCustomValidationKey(configuration, logger);
+        }
+
+        public static void AddDbContexts<TContext>(this IServiceCollection services, IConfiguration configuration) where TContext : DbContext
+        {
+            var connectionString = configuration.GetConnectionString(ConfigurationConsts.AdminConnectionStringKey);
+            //services.AddDbContext<TContext>(options => options.UseSqlServer(connectionString));
+            services.AddDbContext<TContext>(options => options.UseSqlServer(connectionString, optionsSql => { optionsSql.UseRowNumberForPaging(); }));
+        }
+
+        public static void UseMvcLocalizationServices(this IApplicationBuilder app)
+        {
+            var options = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(options.Value);
+        }
+
+        public static void AddLogging(this IApplicationBuilder app, ILoggerFactory loggerFactory, IConfiguration configuration)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+    }
+}
---
+++ src/Skoruba.IdentityServer4.STS.Identity/Startup.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Skoruba.IdentityServer4.Admin.EntityFramework.DbContexts;
+using Skoruba.IdentityServer4.Admin.EntityFramework.Identity.Entities.Identity;
+using Skoruba.IdentityServer4.STS.Identity.Filters;
+using Skoruba.IdentityServer4.STS.Identity.Helpers;
+using StackExchange.Redis;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using WebApiClient.Extensions.DependencyInjection;
+
+namespace Skoruba.IdentityServer4.STS.Identity
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
+        public ILogger Logger { get; set; }
+
+        public Startup(IHostingEnvironment environment, ILoggerFactory loggerFactory)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            if (environment.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+            Environment = environment;
+            Logger = loggerFactory.CreateLogger<Startup>();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var redisDefaultConnection = Configuration["Redis:ConnectionStrings:DefaultConnection"];
+            var redis = ConnectionMultiplexer.Connect(redisDefaultConnection);
+            services.AddDataProtection().SetApplicationName("session_application_name").PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys");
+
+
+            services.AddDistributedServiceStackRedisCache(options =>
+            {
+
+                options.Host = redisDefaultConnection;
+                options.Port = 6379;
+                //options.InstanceName = "redis_session";
+            });
+
+            services.AddDbContexts<AdminDbContext>(Configuration);
+            services.AddAuthenticationServices<AdminDbContext, UserIdentity, UserIdentityRole>(Environment, Configuration, Logger);
+            services.AddMvcLocalization();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequiredUniqueChars = 1;
+            });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder =>
+                    {
+                        builder.AllowAnyHeader().AllowAnyMethod().AllowCredentials().AllowAnyOrigin();
+                    });
+            });
+            services.AddSession(options => { options.IdleTimeout = TimeSpan.FromSeconds(1000); });
+
+            services.AddHttpApi<IUserService>().ConfigureHttpApiConfig((c, p) =>
+            {
+                c.HttpHost = new Uri(Configuration["ApiGateway:BaseUrl"]);
+                c.FormatOptions.DateTimeFormat = "yyyy-MM-dd HH:mm:ss.fff";
+                c.LoggerFactory = p.GetRequiredService<ILoggerFactory>();
+                c.GlobalFilters.Add(new TokenFilter($"{Configuration["ApiGateway:BaseUrl"]}/connect/token", Configuration["Service:Client:Id"], Configuration["Service:Client:Secret"]));
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("IdentityService", new Info { Title = "IdentityService API", Version = "v1" });
+                //c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Skoruba.IdentityServer4.STS.Identity.xml"));
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseCors("AllowAllOrigins");
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "{documentName}/swagger.json";
+            });
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/IdentityService/swagger.json", "IdentityServiceApi");
+            });
+
+            app.AddLogging(loggerFactory, Configuration);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSession();
+            app.UseSecurityHeaders();
+            app.UseStaticFiles();
+            app.UseIdentityServer();
+            app.UseMvcLocalizationServices();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
